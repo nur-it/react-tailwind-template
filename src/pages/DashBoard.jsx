b17ba0316049509de941bc50ccfbe8b873d84a92
Al Amin <alaminpgmr@gmail.com>
2024-08-20 09:39:57 +0600

    added tailwind prettier config

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 import SummeryCard from "../components/screens/dashboard/SummeryCard";
-import Circle from "../components/shared/svg/Circle";
+import { RightArrow } from "../components/shared/svg/RightArrow";
 import Button from "../components/ui/Button";
 
 const DashBoard = () => {
@@ -46,7 +46,7 @@ const DashBoard = () => {
             ))}
           </div>
           <Link to={"/"}>
-            <Button icon={{ Icon: Circle, float: "right" }} className={"mt-6 rounded-full shadow-2xl"}>
+            <Button icon={{ Icon: RightArrow, float: "right" }} className={"mt-6 rounded-full shadow-2xl"}>
               Dynamic Button
             </Button>
           </Link>
