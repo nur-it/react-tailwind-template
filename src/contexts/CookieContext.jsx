efdbae157eb003906dc48222184138850c5316f8
Al Amin <alaminpgmr@gmail.com>
2024-10-03 18:18:59 +0600

    "Updated whitespace and quotes in CookieContext.jsx"

--- src/contexts/CookieContext.jsx
+++ src/contexts/CookieContext.jsx
@@ -1,36 +1,36 @@
-import Cookies from 'js-cookie';
-import { createContext, useContext, useState } from 'react';
+import Cookies from "js-cookie";
+import { createContext, useContext, useState } from "react";
 
 const CookieContext = createContext();
 
 export const useCookies = () => useContext(CookieContext);
 
 const CookieProvider = ({ children }) => {
-	const [cookies, setCookies] = useState(() => ({
-		// Initial cookie state, e.g.,
-		// user: null,
-		// theme: 'light',
-	}));
+  const [cookies, setCookies] = useState(() => ({
+    // Initial cookie state, e.g.,
+    // user: null,
+    // theme: 'light',
+  }));
 
-	const setCookie = (name, value, options) => {
-		Cookies.set(name, value, options);
-		setCookies(prevCookies => ({ ...prevCookies, [name]: value }));
-	};
+  const setCookie = (name, value, options) => {
+    Cookies.set(name, value, options);
+    setCookies((prevCookies) => ({ ...prevCookies, [name]: value }));
+  };
 
-	const removeCookie = name => {
-		Cookies.remove(name);
-		setCookies(prevCookies => ({ ...prevCookies, [name]: undefined }));
-	};
+  const removeCookie = (name) => {
+    Cookies.remove(name);
+    setCookies((prevCookies) => ({ ...prevCookies, [name]: undefined }));
+  };
 
-	const getCookie = name => Cookies.get(name);
+  const getCookie = (name) => Cookies.get(name);
 
-	return (
-		<CookieContext.Provider
-			value={{ cookies, setCookie, removeCookie, getCookie }}
-		>
-			{children}
-		</CookieContext.Provider>
-	);
+  return (
+    <CookieContext.Provider
+      value={{ cookies, setCookie, removeCookie, getCookie }}
+    >
+      {children}
+    </CookieContext.Provider>
+  );
 };
 
 export default CookieProvider;
