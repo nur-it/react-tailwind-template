cba3f0d596870e2361f149b9ea0719095918e8ef
Al Amin <alaminpgmr@gmail.com>
2024-10-05 17:22:07 +0600

    "Updated RoleListsTable component to render a table with headers and a sample row, including an edit icon in the action column."

--- src/components/screens/Role/RoleListsTable.jsx
+++ src/components/screens/Role/RoleListsTable.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { TiEdit } from "react-icons/ti";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHeader,
+  TableRow,
+} from "../../ui/Table";
 
 const RoleListsTable = () => {
-  return <section className="pt-16">RoleListsTable</section>;
+  return (
+    <section className="pt-12">
+      <TableContainer>
+        <Table>
+          <TableHeader>
+            <TableRow className="text-center">
+              <TableCell className="font-bold">Name</TableCell>
+              <TableCell className="font-bold">Description</TableCell>
+              <TableCell className="font-bold">Users</TableCell>
+              <TableCell className="text-center font-bold">Action</TableCell>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow className="text-center">
+              <TableCell>role name</TableCell>
+              <TableCell>role description</TableCell>
+              <TableCell>role hold users</TableCell>
+              <TableCell>
+                <TiEdit className="mx-auto text-xl" />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </section>
+  );
 };
 
 export default RoleListsTable;
